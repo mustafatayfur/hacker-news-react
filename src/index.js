b4578f392a4dc66dbc7651c3e25a6d43eabad646
mustafa <mltm0204@gmail.com>
2022-01-06 23:41:32 +0300

    ...

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { AppProvider } from './context'
+
+
 ReactDOM.render(
+  
   <React.StrictMode>
     <AppProvider>
       <App />
